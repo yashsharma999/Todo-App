0b99e8727574c4808e6af1a3d24e900b7f7ff9e6
Yash Sharma <yashsharma@Yashs-MacBook-Pro.local>
2022-11-11 23:46:36 +0530

    CRUD
    - create all the required CRUD controllers and routers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -2,4 +2,20 @@ const getAllTasks = async (req, res) => {
   res.send("List of all the tasks...");
 };
 
-export { getAllTasks };
+const createTask = async (req, res) => {
+  res.send("Create a task...");
+};
+
+const getTask = async (req, res) => {
+  res.send("Get a task...");
+};
+
+const updateTask = async (req, res) => {
+  res.send("Update a task...");
+};
+
+const deleteTask = async (req, res) => {
+  res.send("Delete a task...");
+};
+
+export { getAllTasks, createTask, getTask, updateTask, deleteTask };
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,14 @@
 import express from "express";
 const router = express.Router();
-import { getAllTasks } from "../controllers/tasks.js";
+import {
+  getAllTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+} from "../controllers/tasks.js";
 
-router.route("/").get(getAllTasks);
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deleteTask);
 
 export default router;
