2236e4f6e6e70df0e4b436ed8eaa7465e56f8434
Yash Sharma <yashsharma@Yashs-MacBook-Pro.local>
2022-11-13 12:50:00 +0530

    Updated schema
    - updated the schema
    - completed all the controller functions

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,24 +1,63 @@
 import Task from "../models/Tasks.js";
 
 const getAllTasks = async (req, res) => {
-  res.send("List of all the tasks...");
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).json({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 };
 
 const getTask = async (req, res) => {
-  res.send("Get a task...");
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 const updateTask = async (req, res) => {
-  res.send("Update a task...");
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 const deleteTask = async (req, res) => {
-  res.send("Delete a task...");
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json(error);
+  }
 };
 
 export { getAllTasks, createTask, getTask, updateTask, deleteTask };
--- models/Tasks.js
+++ models/Tasks.js
@@ -1,8 +1,16 @@
 import mongoose from "mongoose";
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    trim: true,
+    maxlength: [20, "Name cannot be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 export default mongoose.model("Task", TaskSchema);
